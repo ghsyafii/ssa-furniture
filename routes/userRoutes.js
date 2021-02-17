351b3e690aeb7d8ff24d1bb5af9526b10b9f8e2c
ghsyafii <73457694+ghsyafii@users.noreply.github.com>
2021-02-17 21:04:18 +0800

    adding user to db + hashing password works

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+
+//User model
+
+const User = require('../models/user');
 
 router.get('/login', (req,res) => {
     res.render('login', { title: 'Login'});
@@ -30,7 +35,36 @@ router.post('/register', (req,res) => {
             password
         })
     } else {
-        res.send('pass')
+        //validation passed
+        //check if user doesn't exist before adding to db
+        User.findOne({ email: email })
+            .then(user => {
+                if(user) {
+                    errors.push({msg: 'Email is already registered'})
+                    res.render('register', { title: 'Register',
+                        errors,
+                        name,
+                        email,
+                        password
+                    })
+                } else {
+                    const newUser = new User({
+                        name, email, password
+                    })
+                    //hash the password
+                    bcrypt.genSalt(10, (err,salt) => bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if(err) throw err;
+                    //set password to hash
+                        newUser.password = hash;
+                        //save user
+                        newUser.save()
+                            .then(user => {
+                                res.redirect('/users/login');
+                            })
+                            .catch(err => console.log(err))
+                    }))
+                }
+            })
     }
 })
 
