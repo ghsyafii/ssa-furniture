2ca77ed9b151ab9e7fc4d2eb0c4b6b655035a233
ghsyafii <73457694+ghsyafii@users.noreply.github.com>
2021-02-19 16:25:33 +0800

    added cart key to User schema for testing

--- models/user.js
+++ models/user.js
@@ -14,6 +14,10 @@ const userSchema = new Schema({
         type: String,
         required: true
     },
+    cart: {
+        type: Array,
+        required: false
+    }
 }, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
