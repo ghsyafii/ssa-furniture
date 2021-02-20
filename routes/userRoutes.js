ef887ad830f5263e44b2612ee0c941e0768836d9
ghsyafii <73457694+ghsyafii@users.noreply.github.com>
2021-02-20 20:23:27 +0800

    routes for successredirect was wrong - fixed it

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -75,7 +75,7 @@ router.post('/register', (req,res) => {
 
 router.post('/login', (req,res,next) => {
     passport.authenticate('local', {
-        successRedirect: '/products/create',
+        successRedirect: '/',
         failureRedirect: '/users/login',
         failureFlash: true
     })(req,res,next)
